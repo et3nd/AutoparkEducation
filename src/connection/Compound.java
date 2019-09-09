cf93f8174acfa9cbc4bce487cc2e94d9d994786d
alex <luck_112@mail.ru>
2019-09-09 18:39:57 +0300

    AUTO-1 "Database initialization": Rename .sql file Moved the db directory Used stream api Corrected the text

--- src/connection/Compound.java
+++ src/connection/Compound.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.charset.*;
 import java.nio.file.*;
 import java.sql.*;
-import java.util.List;
 
 import org.slf4j.*;
 
@@ -12,26 +11,24 @@ class Compound {
     private static final String URL = "jdbc:h2:file:/home/alex/IdeaProjects/AutoparkEducation/auto park database";
     private static final String LOGIN = "root";
     private static final String PASSWORD = "root";
-    private static StringBuilder sqlScript = new StringBuilder();
-    private final Logger log = LoggerFactory.getLogger(Compound.class);
+    private static final Logger log = LoggerFactory.getLogger(Compound.class);
 
     void interactWithDatabase() {
         try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-             final PreparedStatement preparedStatement = connection.prepareStatement(writeDataFromFile())) {
+             PreparedStatement preparedStatement = connection.prepareStatement(writeDataFromFile())) {
             log.info("Connection to the database was successful");
             preparedStatement.execute();
-            log.info("Database initialization successful");
+            log.info("Database has been successfully initialized");
         } catch (SQLException e) {
             log.error("Error: ", e);
         }
     }
 
     private String writeDataFromFile() {
+        StringBuilder sqlScript = new StringBuilder();
         try {
-            List<String> lines = Files.readAllLines(Paths.get("db/java.sql"), StandardCharsets.UTF_8);
-            for (String line : lines) {
-                sqlScript.append(line);
-            }
+            Files.lines(Paths.get("src/resources/db/TableCreationScript.sql"),
+                    StandardCharsets.UTF_8).forEach(sqlScript::append);
             return sqlScript.toString();
         } catch (IOException e) {
             log.error("Error: ", e);
